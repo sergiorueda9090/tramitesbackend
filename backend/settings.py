b12112fc784a598ca314a309362993767630ec92
sergio rueda <sergiorueda90@hotmail.com>
2026-04-27 20:01:08 -0500

    cambios

--- backend/settings.py
+++ backend/settings.py
@@ -207,7 +207,7 @@ CHANNEL_LAYERS = {
         "CONFIG": {
             "hosts": [(
                 os.getenv('REDIS_HOST', '127.0.0.1'),
-                int(os.getenv('REDIS_PORT', '6379')),
+                int(os.getenv('REDIS_PORT', '6381')),
             )],
         },
     },
